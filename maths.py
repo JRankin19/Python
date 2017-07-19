18cc6122eed17d6d82e839f20bc78d8ee2dd0ec0
James Rankin <jrankin19@wooster.edu>
2017-07-19 19:18:03 -0400

    Does maths

---
+++ maths.py
@@ -0,0 +1,20 @@
+def sum(a, b):
+    sum = a + b
+    return sum
+
+resultOfSum = sum(1, 2)
+#print resultOfSum
+def product(a, b):
+    product = a * b
+    return product
+
+resultOfProduct = product(4, 5)
+#print resultOfProduct
+
+def magic(a, b, c, d):
+    sum = a + b
+    product = sum * c
+    quotient = product // d
+    return quotient
+resultOfMagic = magic(4, 6, 8, 10)
+#print resultOfMagic
